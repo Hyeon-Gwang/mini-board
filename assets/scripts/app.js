76320cee1a1acb050a998848938a09184dbb4ad7
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-01-22 17:09:39 +0900

    포스트 작성 api 추가

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,45 @@
-// axios.defaults.url = 'http://localhost:8080/api';
+const $writeBtn = document.querySelector("#writeBtn");
 
-const $writeBtn = document.querySelector('#writeBtn');
+function getDate() {
+  const today = new Date();
 
-async function writePost() {
-  const title = document.querySelector('#titleInput').value;
-  const content = document.querySelector('#contentInput').value;
-  const password = document.querySelector('#passwordInput').value;
+  const year = today.getFullYear();
+  const month = ('0' + (today.getMonth() + 1)).slice(-2);
+  const day = ('0' + today.getDate()).slice(-2);
 
-  if(title === '') { return alert('제목을 입력하세요.'); };
-  if(content === '') { return alert('내용을 작성하세요.'); };
-  if(password === '') { return alert('비밀번호를 입력하세요.'); }
+  const hours = ('0' + today.getHours()).slice(-2); 
+  const minutes = ('0' + today.getMinutes()).slice(-2);
+  const seconds = ('0' + today.getSeconds()).slice(-2); 
 
-  const result = await axios.post('http://localhost:8080/api/test', {
-    title, content, password,
-  });
-
-  console.log('result: ', result);
+  const fullDate = `${year}${month}${day}${hours}${minutes}${seconds}`
+  return parseInt(fullDate);
 };
 
-$writeBtn.addEventListener('click', writePost);
+function closeModal() {
+  console.log("modal close");
+};
+
+async function writePost() {
+  const title = document.querySelector("#titleInput").value;
+  const content = document.querySelector("#contentInput").value;
+  const writer = document.querySelector("#writerInput").value;
+  const password = document.querySelector("#passwordInput").value;
+  const date = getDate();
+
+  if(title === "") { return alert("제목을 입력하세요."); };
+  if(content === "") { return alert("내용을 작성하세요."); };
+  if(writer === "") { return alert("이름을 입력하세요."); };
+  if(password === "") { return alert("비밀번호를 입력하세요."); };
+
+  const result = await axios.post("http://localhost:8080/api/post/new", {
+    title, content, writer, password, date,
+  });
+
+  if(result.data.result === "success") { 
+    closeModal();
+    window.location.href = "/";
+  };
+  if(result.data.result === "fail") { return alert("포스트 작성에 실패하였습니다.", result.data.error); };
+};
+
+$writeBtn.addEventListener("click", writePost);
--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+const Posts = require("../models/post");
+
 router.get('/posts', (req, res) => {
   res.send('모든 포스트 불러오기')
 });
 
 // 새 포스트 작성 POST /api/post/new
 router.post('/post/new', async (req, res) => {
-  // const { title, content, password } = req.body;
+  try {
+    const { title, content, writer, password, date } = req.body;
 
-  // await miniBoardPosts.create({ title, content, password });
+    let id  = 1;
 
-  // return res.status(201).send({ result: 'success' });
+    const lastPost = await Posts.findOne().sort("-createdAt").exec();
+    if(lastPost) {
+      id = lastPost.id + 1;
+    };
+
+    const newPost = new Posts({ id, title, content, writer, password, createdAt: date, });
+    await newPost.save();
+
+    return res.status(201).send({ result: 'success' });
+  } catch(error) {
+    console.error(error);
+    return res.status(404).send({ result: 'fail', error: error });
+  }
+  
 });
-
-router.post('/test', (req, res) => {
-  const { title, content, password } = req.body;
-
-  console.log('api server:', title, content, password)
-
-  return res.status(201).send({ title, content, password});
-})
 
 module.exports = router;
