250d0d1541f2134d469cc5da2216d2c7d7d96082
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-01-23 17:56:48 +0900

    상세 페이지로 이동하는 함수 추가

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,53 +1,7 @@
-const $writeBtn = document.querySelector("#writeBtn");
 const $searchInput = document.querySelector('#searchInput');
+const $postRows = document.querySelectorAll("tbody > tr");
 
-const editor = new toastui.Editor({
-  el: document.querySelector('#editor'),
-  height: "700px",
-});
-
-function getDate() {
-  const today = new Date();
-
-  const year = today.getFullYear();
-  const month = ('0' + (today.getMonth() + 1)).slice(-2);
-  const day = ('0' + today.getDate()).slice(-2);
-
-  const hours = ('0' + today.getHours()).slice(-2); 
-  const minutes = ('0' + today.getMinutes()).slice(-2);
-  const seconds = ('0' + today.getSeconds()).slice(-2); 
-
-  const fullDate = `${year}${month}${day}${hours}${minutes}${seconds}`
-  return parseInt(fullDate);
-};
-
-// 포스트 작성하기
-async function writePost() {
-  const title = document.querySelector("#titleInput").value;
-  const content = editor.getHTML();
-  const writer = document.querySelector("#writerInput").value;
-  const password = document.querySelector("#passwordInput").value;
-  const date = getDate();
-
-  if(title === "") { return alert("제목을 입력하세요."); };
-  if(content === "") { return alert("내용을 작성하세요."); };
-  if(writer === "") { return alert("이름을 입력하세요."); };
-  if(password === "") { return alert("비밀번호를 입력하세요."); };
-
-  const result = await axios.post("http://localhost:8080/api/post/new", {
-    title, content, writer, password, date,
-  });
-
-  if(result.data.result === "success") { 
-    window.location.href = "/";
-  };
-  if(result.data.result === "fail") { return alert("포스트 작성에 실패하였습니다.", result.data.error); };
-};
-
-$writeBtn.addEventListener("click", writePost);
-
-
-// 포스트 수정하기
+// 포스트 검색하기
 function editPost(e) {
   if(e.keyCode === 13) {
     const searchType = document.querySelector('#searchType').value;
@@ -60,3 +14,13 @@ function editPost(e) {
   }
 }
 $searchInput.addEventListener("keyup", editPost)
+
+// 개별 포스트 페이지로 이동하기
+function movePostPage(e) {
+  const postId = e.target.dataset.id;
+  
+  window.location.href = `/${postId}`;
+};
+$postRows.forEach(postRow => {
+  postRow.addEventListener("click", movePostPage);
+});
