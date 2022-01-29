2e4f0b97db69dd0c9a7e77412b7c9cd59759ad92
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-01-29 21:18:13 +0900

    댓글 작성 함수 작성 & 마크업

---
+++ assets/scripts/comment.js
@@ -0,0 +1,11 @@
+const $commentInput = document.querySelector("#commentInput");
+
+async function addComment(e) {
+  if(e.keyCode === 13) {
+    const comment = $commentInput.value;
+    const date = getDate();
+    
+    await axios.post(`/api/post/${postId}/comment`, { comment, userId: user.id, date });
+  };
+};
+$commentInput.addEventListener("keyup", addComment);
