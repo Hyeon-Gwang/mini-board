bfb16578c646dcc54e4cbd8d74c19dac8d3024ed
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-01-29 21:19:08 +0900

    유저 정보 불러오기 함수 작성 & 로그아웃 함수 합침 &

--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -1,8 +1,16 @@
 const $logoutBtn = document.querySelector("#logoutBtn");
-
 if($logoutBtn) {
   $logoutBtn.addEventListener("click", async () => {
     await axios.delete("/api/auth");
     window.location.href = "/";
   });
 };
+
+// 유저 정보 불러오기
+let user = {}
+async function getUserInfo() {
+  const result = await axios.get("/api/user");
+  user = result.data.user;
+}
+
+getUserInfo();
